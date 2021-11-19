85e0e57931b70833bd5b51c41b3664f87925df39
Danail Valentinov Ivanov <89908414+Danail-Valentinov-Ivanov@users.noreply.github.com>
2021-11-19 03:50:19 +0200

    Add files via upload

---
+++ E2_P1_Class_Box/Box.java
@@ -0,0 +1,56 @@
+package E2_P1_Class_Box;
+
+public class Box {
+    //    - length: double
+//- width: double
+//- height: double
+//+ Box (double length, double width, double height)
+//- setLength(double): void
+//- setWidth(double): void
+//- setHeight(double): void
+//+ calculateSurfaceArea (): double
+//+ calculateLateralSurfaceArea (): double
+//+ calculateVolume (): double
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height) {
+        this.setLength(length);
+        this.setWidth(width);
+        this.setHeight(height);
+    }
+
+    private void setLength(double length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("Length cannot be zero or negative.");
+        }
+        this.length = length;
+    }
+
+    private void setWidth(double width) {
+        if (width <= 0) {
+            throw new IllegalArgumentException("Width cannot be zero or negative.");
+        }
+        this.width = width;
+    }
+
+    private void setHeight(double height) {
+        if ((height <= 0)) {
+            throw new IllegalArgumentException("Height cannot be zero or negative.");
+        }
+        this.height = height;
+    }
+
+    public double calculateSurfaceArea() {
+        return 2 * length * width + 2 * length * height + 2 * width * height;
+    }
+
+    public double calculateLateralSurfaceArea() {
+        return 2 * length * height + 2 * width * height;
+    }
+
+    public double calculateVolume() {
+        return length * width * height;
+    }
+}
