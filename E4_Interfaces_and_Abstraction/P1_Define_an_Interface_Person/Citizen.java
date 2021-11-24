8febbe9a4709173a8ffb425bdb9457985c6586e6
Danail Valentinov Ivanov <89908414+Danail-Valentinov-Ivanov@users.noreply.github.com>
2021-11-25 01:58:04 +0200

    Add files via upload

---
+++ E4_Interfaces_and_Abstraction/P1_Define_an_Interface_Person/Citizen.java
@@ -0,0 +1,21 @@
+package E4_Interfaces_and_Abstraction.P1_Define_an_Interface_Person;
+
+public class Citizen implements Person {
+    private String name;
+    private int age;
+
+    public Citizen(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+}
---
+++ E4_Interfaces_and_Abstraction/P1_Define_an_Interface_Person/Person.java
@@ -0,0 +1,6 @@
+package E4_Interfaces_and_Abstraction.P1_Define_an_Interface_Person;
+
+public interface Person {
+    public String getName();
+    public int getAge();
+}
